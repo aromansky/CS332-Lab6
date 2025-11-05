e50e99732fac88f1221c4b69dd59fba95a3c55e1
Романский Алексей <kirbi.zsthan@gmail.com>
2025-11-05 04:43:09 +0300

    Сделал сцену для создания фигуры вращения

---
+++ CS332-Lab7/RotationFigureForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace CS332_Lab7
+{
+    partial class RotationFigureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel = new Panel();
+            createButton = new Button();
+            clearButton = new Button();
+            xAxisRadioButton = new RadioButton();
+            yAxisRadioButton = new RadioButton();
+            zAxisRadioButton = new RadioButton();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel
+            // 
+            panel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel.BackColor = SystemColors.ActiveBorder;
+            panel.Location = new Point(12, 12);
+            panel.Name = "panel";
+            panel.Size = new Size(635, 426);
+            panel.TabIndex = 0;
+            panel.Paint += panel_Paint;
+            panel.MouseClick += panel_MouseClick;
+            // 
+            // createButton
+            // 
+            createButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            createButton.Location = new Point(653, 181);
+            createButton.Name = "createButton";
+            createButton.Size = new Size(75, 23);
+            createButton.TabIndex = 1;
+            createButton.Text = "Создать";
+            createButton.UseVisualStyleBackColor = true;
+            createButton.Click += createButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            clearButton.Location = new Point(653, 12);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(75, 23);
+            clearButton.TabIndex = 2;
+            clearButton.Text = "Очистить";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // xAxisRadioButton
+            // 
+            xAxisRadioButton.AutoSize = true;
+            xAxisRadioButton.Location = new Point(653, 41);
+            xAxisRadioButton.Name = "xAxisRadioButton";
+            xAxisRadioButton.Size = new Size(56, 19);
+            xAxisRadioButton.TabIndex = 3;
+            xAxisRadioButton.Text = "Ось X";
+            xAxisRadioButton.UseVisualStyleBackColor = true;
+            xAxisRadioButton.CheckedChanged += xAxisRadioButton_CheckedChanged;
+            // 
+            // yAxisRadioButton
+            // 
+            yAxisRadioButton.AutoSize = true;
+            yAxisRadioButton.Checked = true;
+            yAxisRadioButton.Location = new Point(653, 66);
+            yAxisRadioButton.Name = "yAxisRadioButton";
+            yAxisRadioButton.Size = new Size(56, 19);
+            yAxisRadioButton.TabIndex = 4;
+            yAxisRadioButton.TabStop = true;
+            yAxisRadioButton.Text = "Ось Y";
+            yAxisRadioButton.UseVisualStyleBackColor = true;
+            yAxisRadioButton.CheckedChanged += yAxisRadioButton_CheckedChanged;
+            // 
+            // zAxisRadioButton
+            // 
+            zAxisRadioButton.AutoSize = true;
+            zAxisRadioButton.Location = new Point(653, 91);
+            zAxisRadioButton.Name = "zAxisRadioButton";
+            zAxisRadioButton.Size = new Size(56, 19);
+            zAxisRadioButton.TabIndex = 5;
+            zAxisRadioButton.Text = "Ось Z";
+            zAxisRadioButton.UseVisualStyleBackColor = true;
+            zAxisRadioButton.CheckedChanged += zAxisRadioButton_CheckedChanged;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(653, 138);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(75, 23);
+            numericUpDown1.TabIndex = 6;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(653, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Кол-во разбиений";
+            // 
+            // RotationFigureForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(zAxisRadioButton);
+            Controls.Add(yAxisRadioButton);
+            Controls.Add(xAxisRadioButton);
+            Controls.Add(clearButton);
+            Controls.Add(createButton);
+            Controls.Add(panel);
+            Name = "RotationFigureForm";
+            Text = "Создание образующей";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel;
+        private Button createButton;
+        private Button clearButton;
+        private RadioButton xAxisRadioButton;
+        private RadioButton yAxisRadioButton;
+        private RadioButton zAxisRadioButton;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+    }
+}
---
+++ CS332-Lab7/RotationFigureForm.cs
@@ -0,0 +1,155 @@
+﻿using Geometry;
+using System.Drawing;
+
+namespace CS332_Lab7
+{
+    public partial class RotationFigureForm : Form
+    {
+        public int numParts = 0;
+        public enum Axis { X, Y, Z }
+        public Axis rotationAxis = Axis.Y;
+        private List<PointF> profilePoints;
+        public Polyhedron poly;
+        public RotationFigureForm()
+        {
+            InitializeComponent();
+            profilePoints = new List<PointF>();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (profilePoints == null || profilePoints.Count < 2 || numParts < 3)
+            {
+                MessageBox.Show("Неверные исходные данные для генерации полиэдра.");
+                return;
+            }
+
+            List<List<Point3D>> rings = new List<List<Point3D>>();
+            float angleStep = 360f / numParts;
+
+            for (int i = 0; i < numParts; i++)
+            {
+                float radians = i * angleStep * (float)(Math.PI / 180);
+                Matrix rotationMatrix = Transform.CreateRotationAroundYMatrix(radians);
+
+                List<Point3D> currentRing = new List<Point3D>();
+                foreach (PointF point in profilePoints)
+                {
+                    Point3D originalPoint = new Point3D(point.X - 0.5f, point.Y, 0);
+                    Point3D rotatedPoint = Transform.Apply(rotationMatrix, originalPoint);
+                    currentRing.Add(rotatedPoint);
+                }
+                rings.Add(currentRing);
+            }
+
+            List<Face> faces = new List<Face>();
+            for (int i = 0; i < numParts; i++)
+            {
+                int nextIndex = (i + 1) % numParts;
+                for (int j = 0; j < profilePoints.Count - 1; j++)
+                {
+                    Point3D p1 = rings[i][j];
+                    Point3D p2 = rings[nextIndex][j];
+                    Point3D p3 = rings[nextIndex][j + 1];
+                    Point3D p4 = rings[i][j + 1];
+
+                    faces.Add(new Face(new List<Edge>
+            {
+                new Edge(p1, p2),
+                new Edge(p2, p3),
+                new Edge(p3, p4),
+                new Edge(p4, p1)
+            }));
+                }
+            }
+
+            this.poly = new Polyhedron(faces);
+
+            Point3D center = poly.GetCenter();
+            Matrix translateMatrix = Transform.CreateTranslationMatrix(-center.X, -center.Y, -center.Z);
+            this.poly = Transform.Apply(translateMatrix, poly);
+
+            Matrix rotationMatrixFinal;
+            switch (rotationAxis)
+            {
+                case Axis.X:
+                    rotationMatrixFinal = Transform.CreateRotationAroundXMatrix((float)Math.PI / 2);
+                    break;
+                case Axis.Y:
+                    rotationMatrixFinal = Matrix.Identity(); 
+                    break;
+                default: // Z
+                    rotationMatrixFinal = Transform.CreateRotationAroundZMatrix((float)Math.PI / 2);
+                    break;
+            }
+            this.poly = Transform.Apply(rotationMatrixFinal, this.poly);
+
+            this.DialogResult = DialogResult.OK;
+        }
+
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            xAxisRadioButton.Checked = true;
+            numericUpDown1.Value = 0;
+            profilePoints.Clear();
+
+            panel.Invalidate();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(SystemColors.ActiveBorder);
+
+            using (Pen p = new Pen(Color.Red, 4))
+            {
+                int axisX = panel.Width / 2;
+                g.DrawLine(p, axisX, 0, axisX, panel.Height);
+            }
+
+            using (Pen p = new Pen(Color.Blue, 2))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                foreach (PointF point in profilePoints)
+                {
+                    g.FillEllipse(Brushes.Red, point.X * panel.Width - 3, point.Y * panel.Height - 3, 6, 6);
+                }
+
+                if (profilePoints.Count > 1)
+                {
+                    g.DrawLines(p, profilePoints.Select(x => new PointF(x.X * panel.Width, x.Y * panel.Height)).ToArray());
+                }
+            }
+        }
+
+        private void xAxisRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (xAxisRadioButton.Checked) rotationAxis = Axis.X;
+        }
+
+        private void yAxisRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (yAxisRadioButton.Checked) rotationAxis = Axis.Y;
+        }
+
+        private void zAxisRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (zAxisRadioButton.Checked) rotationAxis = Axis.Z;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numParts = (int)numericUpDown1.Value;
+        }
+
+        private void panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Math.Abs(e.X - panel.Width / 2) < 3)
+                profilePoints.Add(new PointF(0.5f, e.Y * 1f / panel.Height));
+            else
+                profilePoints.Add(new PointF(e.X * 1f / panel.Width, e.Y * 1f / panel.Height));
+            panel.Invalidate();
+        }
+    }
+}
